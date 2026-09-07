0c5fca5af1a0922559d3d65969342171e9a17579
mdfaizalam001 <mohammadfaizalam503@gmail.com>
2026-09-07 16:55:16 +0530

    Time: 6 ms (87.41%) | Memory: 44.3 MB (32.90%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            count1[s1.charAt(i) - 'a']++;
+        }
+
+        int k = s1.length();
+
+        for (int i = 0; i < k; i++) {
+            count2[s2.charAt(i) - 'a']++;
+        }
+
+        if (matches(count1, count2)) {
+            return true;
+        }
+
+        for (int right = k; right < s2.length(); right++) {
+            count2[s2.charAt(right) - 'a']++;
+
+            int left = right - k;
+            count2[s2.charAt(left) - 'a']--;
+
+            if (matches(count1, count2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean matches(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
